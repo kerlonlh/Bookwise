5020fb442ca790bf27bfc4fcdc8fe111b54f326a
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2024-12-05 20:26:27 -0300

    Criando nossas primeiras views

---
+++ views/index.view.php
@@ -0,0 +1,24 @@
+<form class="w-full flex space-x-2 mt-6">
+    <input type="text" class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-nome px-2 py-1"
+        placeholder="Pesquisar..." name="pesquisar" />
+    <button type="submit">🔎</button>
+</form>
+<!-- Lista de livros -->
+<section class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <?php foreach($livros as $livro): ?>
+    <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+        <div class="flex">
+            <div class="w-1/3">imagem</div>
+            <div class="space-y-1">
+                <a href="/livro.php?id=<?=$livro['id']?>"
+                    class="font-semibold hover:underline"><?=$livro['titulo']?></a>
+                <div class="text-xs italic"><?=$livro['autor']?></div>
+                <div class="text-xs italic">⭐⭐⭐⭐⭐</div>
+            </div>
+        </div>
+        <div class="text-sm mt-2">
+            <?=$livro['descricao']?>
+        </div>
+    </div>
+    <?php endforeach; ?>
+</section>
