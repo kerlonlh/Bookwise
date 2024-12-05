2c893ef5e4e0ee71b059396205c1450390296bb8
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2024-12-05 20:52:24 -0300

    Criando os nossos controllers

--- views/index.view.php
+++ views/index.view.php
@@ -10,7 +10,7 @@
         <div class="flex">
             <div class="w-1/3">imagem</div>
             <div class="space-y-1">
-                <a href="/livro.php?id=<?=$livro['id']?>"
+                <a href="/livro?id=<?=$livro['id']?>"
                     class="font-semibold hover:underline"><?=$livro['titulo']?></a>
                 <div class="text-xs italic"><?=$livro['autor']?></div>
                 <div class="text-xs italic">⭐⭐⭐⭐⭐</div>
