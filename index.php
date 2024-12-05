2e088c8859b0330450666319dae78c857b171a7d
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2024-12-04 21:49:39 -0300

    Superglobais

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php 
+require 'dados.php';
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -31,71 +36,22 @@
         <!-- Lista de livros -->
         <section class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             <!-- livro -->
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-                <div class="flex">
-                    <div class="w-1/3">imagem</div>
-                    <div>
-                        <a href="/livro.php?" class="font-semibold hover:underline">titulo</a>
-                        <div class="text-xs italic">autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐</div>
+            <?php foreach($livros as $livro): ?>
+                <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+                    <div class="flex">
+                        <div class="w-1/3">imagem</div>
+                        <div class="space-y-1">
+                            <a href="/livro.php?id=<?=$livro['id']?>"
+                                class="font-semibold hover:underline"><?=$livro['titulo']?></a>
+                            <div class="text-xs italic"><?=$livro['autor']?></div>
+                            <div class="text-xs italic">⭐⭐⭐⭐⭐</div>
+                        </div>
+                    </div>
+                    <div class="text-sm mt-2">
+                        <?=$livro['descricao']?>
                     </div>
                 </div>
-                <div class="text-sm">
-                    descrição
-                </div>
-            </div>
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-                <div class="flex">
-                    <div class="w-1/3">imagem</div>
-                    <div>
-                        <a href="/livro.php?" class="font-semibold hover:underline">titulo</a>
-                        <div class="text-xs italic">autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐</div>
-                    </div>
-                </div>
-                <div class="text-sm">
-                    descrição
-                </div>
-            </div>
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-                <div class="flex">
-                    <div class="w-1/3">imagem</div>
-                    <div>
-                        <a href="/livro.php?" class="font-semibold hover:underline">titulo</a>
-                        <div class="text-xs italic">autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐</div>
-                    </div>
-                </div>
-                <div class="text-sm">
-                    descrição
-                </div>
-            </div>
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-                <div class="flex">
-                    <div class="w-1/3">imagem</div>
-                    <div>
-                        <a href="/livro.php?" class="font-semibold hover:underline">titulo</a>
-                        <div class="text-xs italic">autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐</div>
-                    </div>
-                </div>
-                <div class="text-sm">
-                    descrição
-                </div>
-            </div>
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-                <div class="flex">
-                    <div class="w-1/3">imagem</div>
-                    <div>
-                        <a href="/livro.php?" class="font-semibold hover:underline">titulo</a>
-                        <div class="text-xs italic">autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐</div>
-                    </div>
-                </div>
-                <div class="text-sm">
-                    descrição
-                </div>
-            </div>
+            <?php endforeach; ?>
         </section>
     </main>
 </body>
--- livro.php
+++ livro.php
@@ -1,3 +1,12 @@
+<?php 
+require 'dados.php';
+$id = $_REQUEST['id'];
+
+$filtrado = array_filter($livros, fn($l) => $l['id'] == $id);
+$livro = array_pop($filtrado);
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -7,7 +16,6 @@
     <title>Book Wise</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-
 <body class="bg-stone-950 text-stone-200">
     <header class="bg-stone-900">
         <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
@@ -21,7 +29,24 @@
             </ul>
         </nav>
     </header>
-    muda aqui
+<main class="mx-auto max-w-screen-lg space-y-6">
+    <!-- Lista de livros -->
+
+        <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+            <div class="flex">
+                <div class="w-1/3">imagem</div>
+                <div class="space-y-1">
+                    <a href="/livro.php?id=<?=$livro['id']?>"
+                        class="font-semibold hover:underline"><?=$livro['titulo']?></a>
+                    <div class="text-xs italic"><?=$livro['autor']?></div>
+                    <div class="text-xs italic">⭐⭐⭐⭐⭐</div>
+                </div>
+            </div>
+            <div class="text-sm mt-2">
+                <?=$livro['descricao']?>
+            </div>
+        </div>
+</main>
 </body>
 
 </html>
