219b7bf073825152233636d1d737ee3119a0bd29
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2024-12-06 09:08:40 -0300

    Criando um roteador

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php 
 
-$controller = "index";
-if (isset($_SERVER['PATH_INFO'])){
-    $controller = str_replace ('/', '', $_SERVER['PATH_INFO']);
-};
-require "controllers/{$controller}.controller.php";
+require 'functions.php';
+require 'routes.php';
+
 ?>
 
 
