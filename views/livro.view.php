6307b910b95328f25defc29b7f8d4dc9475c630d
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2024-12-07 13:06:58 -0300

    Acessando os dados do banco com PHP

--- views/livro.view.php
+++ views/livro.view.php
@@ -2,7 +2,7 @@
     <div class="flex">
         <div class="w-1/3">imagem</div>
         <div class="space-y-1">
-            <a href="/livro.php?id=<?=$livro['id']?>" class="font-semibold hover:underline"><?=$livro['titulo']?></a>
+            <a href="/livro?id=<?=$livro['id']?>" class="font-semibold hover:underline"><?=$livro['titulo']?></a>
             <div class="text-xs italic"><?=$livro['autor']?></div>
             <div class="text-xs italic">⭐⭐⭐⭐⭐</div>
         </div>
