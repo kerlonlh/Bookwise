0c50e7a4dacb2efe8634ba7448b612ac59401a55
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-06-11 23:08:13 -0300

    Arrumando as primeiras views

--- views/index.view.php
+++ views/index.view.php
@@ -1,1 +1,10 @@
-LockBox
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content">
+    <div>
+    <p class="py-2">Bem vindo ao</p>
+      <h1 class="text-5xl font-bold">LockBox</h1>
+      <p class="py-4"> onde você guarda <span class="italic">tudo</span> com segurança.</p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
